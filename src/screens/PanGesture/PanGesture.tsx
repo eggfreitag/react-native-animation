94f443c1f3f0fea8467ba784662555807bf2ba80
Wonhee Kang <eggfreitag@gmail.com>
2023-02-24 14:03:55 +0900

    PanGestureHandler를 이용하여 카드 이동 구현

--- src/screens/PanGesture/PanGesture.tsx
+++ src/screens/PanGesture/PanGesture.tsx
@@ -1,10 +1,53 @@
 import React from "react";
-import { View, Text } from "react-native";
+import Animated, {
+  useSharedValue,
+  useAnimatedGestureHandler,
+  useAnimatedStyle,
+} from "react-native-reanimated";
+import { PanGestureHandler } from "react-native-gesture-handler";
+import { View, LayoutRectangle, StyleSheet } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
-const PanGesture = () => {
+import Card, { Cards, CARD_HEIGHT, CARD_WIDTH } from "../../components/Card";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    borderBottomWidth: 1,
+  },
+});
+
+const PanGesture = ({
+  width: windowWidth,
+  height: windowHeight,
+}: LayoutRectangle) => {
+  const { bottom: marginBottom } = useSafeAreaInsets();
+  const boundX = windowWidth - CARD_WIDTH;
+  const boundY = windowHeight - CARD_HEIGHT;
+  const translateX = useSharedValue(0);
+  const translateY = useSharedValue(0);
+
+  const onGestureEvent = useAnimatedGestureHandler({
+    onActive: ({ translationX, translationY }) => {
+      translateX.value = translationX;
+      translateY.value = translationY;
+    },
+  });
+
+  const style = useAnimatedStyle(() => ({
+    transform: [
+      { translateX: translateX.value },
+      { translateY: translateY.value },
+    ],
+  }));
+
   return (
-    <View>
-      <Text>PanGesture</Text>
+    <View style={[styles.container, { marginBottom }]}>
+      <PanGestureHandler {...{ onGestureEvent }}>
+        <Animated.View style={[style]}>
+          <Card card={Cards.Card4} />
+        </Animated.View>
+      </PanGestureHandler>
     </View>
   );
 };
