4304554bf377bd0189e99379f4f61b28097ad16f
Wonhee Kang <eggfreitag@gmail.com>
2023-03-02 15:24:42 +0900

    스택 생성

---
+++ src/screens/HigherOrderAnimation/HigherOrderAnimation.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const HigherOrderAnimation = () => {
+  return (
+    <View>
+      <Text>HigherOrderAnimation</Text>
+    </View>
+  );
+};
+
+export default HigherOrderAnimation;
