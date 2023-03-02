3399a8779c1c7e31a4ac7b38373020e476add1bb
Wonhee Kang <eggfreitag@gmail.com>
2023-03-02 15:10:20 +0900

    Transition 애니메이션 구현 완료

---
+++ src/constants.ts
@@ -0,0 +1,25 @@
+const StyleGuide = {
+  spacing: 8,
+  palette: {
+    primary: "#3884ff",
+    secondary: "#FF6584",
+    tertiary: "#38ffb3",
+    backgroundPrimary: "#d5e5ff", // === rgba(primary, 0.1)
+    background: "#f2f2f2",
+    border: "#f2f2f2",
+  },
+  title1: {
+    fontSize: 34,
+    lineHeight: 41,
+  },
+  title2: {
+    fontSize: 28,
+    lineHeight: 34,
+  },
+  title3: {
+    fontSize: 22,
+    lineHeight: 26,
+  },
+};
+
+export default StyleGuide;
